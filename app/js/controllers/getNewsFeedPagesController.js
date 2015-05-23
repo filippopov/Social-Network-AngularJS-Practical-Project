51debb2fde6d2214ebc76f0f89448616d37afec9
filippopov <filip_popov@mail.bg>
2015-05-23 11:08:03 +0300

    view posts
    view posts

--- app/js/controllers/getNewsFeedPagesController.js
+++ app/js/controllers/getNewsFeedPagesController.js
@@ -4,6 +4,15 @@ app.controller('getNewsFeedPagesController', function getNewsFeedPagesController
         .$promise
         .then(function(data){
             $scope.pagesInfo=data;
+            var like=data[1].liked;
+            function isLike(like){
+                if(like){
+                    return true;
+                }
+                return false;
+            }
+            $scope.isLike=isLike(like)
+            console.log($scope.isLike);
             console.log(data);
             $scope.myFriends=data;
         },function(error){
